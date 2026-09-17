7df4816ccd67796606a73124d6f9e9809c499fb7
Christian Guzman <christian.guzman.exec@gmail.com>
2026-09-17 23:15:08 +0100

    chore(a11y): add MVP accessibility gates
    Closes #16

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,19 @@ import tsParser from '@typescript-eslint/parser';
 import astro from 'eslint-plugin-astro';
 import react from 'eslint-plugin-react';
 import hooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default [
   { ignores: ['dist/**', '.astro/**', 'coverage/**', 'storybook-static/**', 'storybook-build/**'] },
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: { parser: tsParser },
-    plugins: { '@typescript-eslint': tseslint, react, 'react-hooks': hooks },
+    plugins: { '@typescript-eslint': tseslint, react, 'react-hooks': hooks, 'jsx-a11y': jsxA11y },
     rules: {
       ...tseslint.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...hooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
     },
     settings: { react: { version: 'detect' } },
